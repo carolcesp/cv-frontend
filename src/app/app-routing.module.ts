b842fa0e5c39188c7d3d4e8a1244e3b8cefb4b06
carolcesp <carolinacespedes90@gmail.com>
2020-05-15 14:58:46 +0200

    initial project cv forntend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from "./components/home/home.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { ListProjectsComponent } from "./components/list-projects/list-projects.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , component: HomeComponent, pathMatch: 'full'},
+  {path:'contact' , component: ContactComponent},
+  {path: 'projects/:id' , component: ListProjectsComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
